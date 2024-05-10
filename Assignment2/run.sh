17eba1818dd19b0cd8ff309d54e9d95b1ca829ca
Chenrui Fan <chenrui_fan@outlook.com>
2024-05-10 22:10:58 +0200

    update

---
+++ Assignment2/run.sh
@@ -0,0 +1,8 @@
+python train.py --device-id=0 --config-file-path=./configs/config_model_1.yaml \
+--experiment-name=model_1 --num-epochs=100
+
+python train.py --device-id=0 --config-file-path=./configs/config_model_2.yaml \
+--experiment-name=model_2 --num-epochs=100
+
+python train.py --device-id=0 --config-file-path=./configs/config_model_3.yaml \
+--experiment-name=model_3 --num-epochs=100
